bef0c24eaf376c32d9e8b8931f67c4d8d85b128a
Тимчасовий обліковий запис Student <student@kpnu.km.ua>
2017-08-30 14:35:24 +0300

    gogogo

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var router = express.Router();
 
+var credentials = require('../credentials.js');
+var emailService = require('../lib/email.js')(credentials);
+
 router.get('/', function(req, res) {
 	console.log('--------/sendMail'.cyan);
 	res.render('sendMail');
@@ -13,12 +16,14 @@ router.post('/', function(req, res) {
     var name = req.body.name;
     var mail = req.body.mail;
     var message = req.body.message;
+    var html = '<h1>123</h1>';
 
     console.log('name ' + name);
     console.log('mail ' + mail);
     console.log('message ' + message);
 
     emailService.send(mail, message, html);
+    console.log(' MESSAGE_SEND to --->>'.black.bgYellow + mail );
 
     res.render('home');
 

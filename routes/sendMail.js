32a914c6b5a810fb208438af59a2c4cd7a0981d2
Alex <izofatov2007@gmail.com>
2017-09-05 22:21:25 +0300

    YEP))

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -4,6 +4,9 @@ var router = express.Router();
 var credentials = require('../credentials.js');
 var emailService = require('../lib/email.js')(credentials);
 
+var global = "0";
+var globalcolls = 0;
+
 router.get('/', function(req, res) {
 	console.log('--------/sendMail'.cyan);
 	res.render('sendMail');
@@ -25,26 +28,102 @@ router.post('/', function(req, res) {
     try {
 
         console.log(' MESSAGE START SENDING from my site --->>'.black.bgYellow);
-        //Send to me
-        emailService.send(credentials.gmail.user, message, html, collback = "null");
-        console.log(collback);
-        // if(info == "1234"){console.log('1234');}
-        //Send to user
-        // var html = '<h3>' + name + ', дякую за підписку! </h3>';
-        // emailService.send(mail, message, html);
 
-        console.log(' MESSAGE_SENDed to me --->>'.black.bgYellow + credentials.gmail.user );
-        console.log(' MESSAGE_SENDed to user --->>'.black.bgYellow + mail );
+        var temp = "temp";
 
-        res.render('thankYou');
-        console.log('------/thankYou'.cyan);
+        function sendFirstMessage() {
+            return new Promise((resolve, reject) => {
+                console.log('Promis 1 -------------------------------------- start');
+                //Send to me
+                emailService.send(credentials.gmail.user, message, html, function(callback) {
+                    temp = callback;
+                    console.log("temp in callback = " + temp);
+                    resolve(temp);
+                })
+            })
+        };
+
+        function sendSecondMessage() {
+            return new Promise((resolve, reject) => {
+                console.log('Promis 2 --------------------------------------- start');
+                //   Send to user
+                var html = '<h3>' + name + ', дякую за підписку! </h3>';
+                emailService.send(mail, message, html, function(callback){
+                    temp = callback;
+                    console.log("temp in callback2 = " + temp);
+                    resolve(temp);
+                })
+            })
+        };
+
+                    // });
+            function checkSend() {
+                return new Promise (function(resolve, reject) {
+                    console.log("temp in callback = " + temp);
+                    if(temp == "1") {
+                        console.log('------/thankYou'.cyan);
+                        resolve('thankYou');
+                    } else {
+                        console.log('reject ->');
+                        reject('error4ick');
+                    }
+                })
+            }
+
+            function backToUser() {
+                return new Promise(function(resolve, reject) {
+                    console.log("backToUser")
+                    if(temp == "1"){
+                        res.render('thankYou');
+                        console.log(' MESSAGE_SENDed to me --->>'.black.bgYellow + credentials.gmail.user );
+                        console.log(' MESSAGE_SENDed to user --->>'.black.bgYellow + mail );
+                    } else {
+                        res.render('sendMailErr');
+                    }
+                })
+            }
+
+
+                // if(global == "1"){
+                //     console.log("+global = " + global);
+                //     // Send to user
+                //     // var html = '<h3>' + name + ', дякую за підписку! </h3>';
+                //     // emailService.send(mail, message, html);
+                //     console.log(' MESSAGE_SENDed to me --->>'.black.bgYellow + credentials.gmail.user );
+                //     console.log(' MESSAGE_SENDed to user --->>'.black.bgYellow + mail );
+                //     res.render('thankYou');
+                //     console.log('------/thankYou'.cyan);
+                //     // resolve(global);
+                // } else {
+                //     console.log('reject ->');
+                //     // reject(global);
+                // }
+
+
+
+        
+        sendFirstMessage()
+        .then(function(value){console.log("value = " + value); return value;})
+          .then(checkSend)
+            .then(sendSecondMessage)
+                .then(checkSend)
+                    .then(backToUser)
+
+
+            .catch(function(value){console.log('catch = ' + value);
+                    res.render('sendMailErr');
+            });
+
+
+
 
     } catch (e) {
         console.log('trable here ---> try/catch sendMail.js: '.red + e);
+        console.log('------/sendMailErr'.cyan);
         res.render('sendMailErr');
     }
 
-    console.log('sendMails dane!'.green);
+    console.log('sendMails end!'.green);
 });
 
 module.exports = router;
